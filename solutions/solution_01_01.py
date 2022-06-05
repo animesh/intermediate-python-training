08c24c8dfe5124fba3af8bdb5aaee45e3589f0d9
Robin Engler <robin.engler@gmail.com>
2022-06-05 19:11:34 +0200

    refactoring in progress

--- solutions/solution_01_01.py
+++ solutions/solution_01_01.py
@@ -1,22 +1,48 @@
-# 1. Select passengers which survived. How many are males/females?
-m_survived = df.Survived == 1
-m_survived.value_counts()
-#2. Create a new column Title is the DataFrame representing the title by which passengers should be addressed. The title can be found in the passenger name and is the only word ending with a '.'
+import pandas as pd
 
-def getTitle( name ):
-    ''' takes a name and return the appropriate title, which is the only word in the name ending with a "."'''
-    splitted_name = name.split()
-    for word in splitted_name:
-        if word.endswith('.'):
+# Load the data as DataFrame.
+df = pd.read_table("data/titanic.csv", sep=",")
+
+# 1. Select passengers which survived. How many are males/females?
+survived = df.Survived == 1
+survivors = df.loc[survived, "Sex"].value_counts()
+for gender in survivors.index:
+    print(f"Number of surviving {gender}: {survivors[gender]}")
+
+# Bonus: compute the survival rates among women/men.
+# Note: "survival_rate:.1f" allows to format the number so it becomes rounded
+# at 1 digit after the decimal.
+for gender in survivors.index:
+    survival_rate = survivors[gender] / sum(df["Sex"] == gender) * 100
+    print(f"Number of surviving {gender}: {survivors[gender]} [{survival_rate:.1f}%]")
+
+
+# 2. Create a new column named "Title" in the DataFrame, representing
+#    the title by which passengers should be addressed. The title can
+#    be found in the passenger name and is the only word ending with a '.'
+
+# First we create a short function that extracts a passenger title from
+# its name.
+def get_passenger_title(name):
+    """Takes a name and return the appropriate title. Only works if
+    the title is the only word in the name ending with a "."
+    """
+    split_name = name.split()
+    for word in split_name:
+        if word.endswith("."):
             return word
     else:
-        return 'No title'
+        return 'no_title'
     
-## use our function on each name to create a new column
-df["Title"] = [getTitle(name) for name in df.Name]
+# Use our function on each name to create the new "Title" column.
+df["Title"] = [get_passenger_title(name) for name in df["Name"]]
+df.head()
 
-## alternatively, this can also be written using the apply method,
-## which applies a function to each element in a column:
-df["Title"] = df.Name.apply(getTitle)
+# To quickly check the list of unique values stored in "Title" (can be
+# useful to check if we have a bad value in our column).
+print(set(df["Title"]))
 
+# Alternatively, this can also be written using the pandas "apply()"
+# method, which applies a function to each element of a column:
+df["Title"] = df["Name"].apply(get_passenger_title)
 df.head()
--- solutions/solution_01_02.py
+++ solutions/solution_01_02.py
@@ -1,16 +1,28 @@
-# 1. Plot the Age distribution among first class passengers. Try to choose an appropriate mode of representation (histogram? density line? number of bins?)
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
 
-# here I prefer the histogram
-## bins can be a number, but also a set of boundaries between bins
-# using np.arange, I get my bins to be 2.5 years wide 
-sns.histplot( df.Age , bins= np.arange(0,85,2.5) ) 
-#2. Make a figure with 3 panels. In the panels, plot the histogram of the `Fare` among passengers in
-#   the first, second, and third class, respectively.
+# Load the data as DataFrame.
+df = pd.read_table("data/titanic.csv", sep=",")
 
-fig, ax = plt.subplots(3,1 , figsize=(10,10))
+# 1. Plot the age distribution among first class passengers. Try to choose an
+# appropriate mode of representation (histogram? density line? number of bins?)
+
+# To display the data, we will here use histograms. To define the histogram
+# bins, we can either give a number (of bins), or a set of boundaries between
+# bins using np.arange(), I get my bins to be 2.5 years wide 
+sns.histplot(df["Age"], bins= np.arange(0, 85, 2.5)) 
+
+
+# 2. Create a figure with 3 panels. In the panels, plot the histogram of the
+# `Fare` among passengers in first, second, and third class, respectively.
+fig, ax = plt.subplots(3, 1, figsize=(10,10))
 
 for i, pclass in enumerate([1,2,3]):
-    sns.histplot( df.loc[ df.Pclass == pclass, 'Fare'] , ax = ax[i] )
+    sns.histplot( df.loc[df["Pclass"] == pclass, 'Fare'] , ax = ax[i])
     ax[i].set_title("passenger class {}".format(pclass))
-plt.tight_layout() # this makes the different subplot organize better, 
-# with no collision between labels, titles, ...
+
+# Makes the different subplot organize better - no collision between labels,
+# titles, etc.
+plt.tight_layout() 
